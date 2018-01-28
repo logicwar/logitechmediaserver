201460c76f242926c82904740840356784ff8095
logicwar <35894092+logicwar@users.noreply.github.com>
2018-01-28 21:19:32 +0100

    Add files via upload

---
+++ root/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#########################################
+
+##        ENVIRONMENTAL CONFIG         ##
+
+#########################################
+
+# Disable unecessary services (SSH, Syslog and Cron)
+rm -rf /etc/service/sshd /etc/service/cron /etc/service/syslog-ng /etc/my_init.d/00_regen_ssh_host_keys.sh /etc/my_init.d/10_syslog-ng.init
+
+
+#########################################
+
+##           CREATE FOLDERS            ##
+
+#########################################
+# Media folders
+mkdir \
+	/music \
+	/playlist
+
+# Make sure the "squeezeboxserver" script will not create them with 'root' ownership later
+mkdir -p \
+	/config/logs \
+	/config/prefs/plugin \
+	/config/cache/audioUploads \
+	/config/cache/InstalledPlugins \
+	/config/cache/DownloadedPlugins \
+	/config/cache/updates
+
+touch /config/logs/perfmon.log
+touch /config/logs/server.log
+
+
+#########################################
+
+##          SET PERMISSIONS            ##
+
+#########################################
+# create a "docker" user
+useradd -U docker
+
+# set proper permissions
+usermod -aG audio docker
+chown -R docker:docker \
+	/config
+chown docker:docker \
+	/music \
+	/playlist
+
